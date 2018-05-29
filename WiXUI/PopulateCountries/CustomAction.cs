e5890909a838456243621c847551c395e1864cda
Matt <mathu99@gmail.com>
2018-05-29 10:51:23 +0200

    feat: pull ComboBox values from server

--- WiXUI/PopulateCountries/CustomAction.cs
+++ WiXUI/PopulateCountries/CustomAction.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Deployment.WindowsInstaller;
+using Newtonsoft.Json;
 
 namespace PopulateCountries
 {
     public class CustomActions
     {
-        static int intex = 1;
+        static int index = 1;
+        private static readonly HttpClient client = new HttpClient();
         public static void FillComboBox(Session session, string text, string value)
         {
             View view = session.Database.OpenView("SELECT * FROM ComboBox");
@@ -15,22 +19,49 @@ namespace PopulateCountries
 
             Record record = session.Database.CreateRecord(4);
             record.SetString(1, "COUNTRIES");
-            record.SetInteger(2, intex);
+            record.SetInteger(2, index);
             record.SetString(3, value);
             record.SetString(4, text);
 
             view.Modify(ViewModifyMode.InsertTemporary, record);
             view.Close();
-            intex++;
+            index++;
         }
+
+        private static async Task<string> getResponseAsync()
+        {
+            var url = "http://localhost:9998/api/servers";
+            var responseMsg = "Error";
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                responseMsg = await response.Content.ReadAsStringAsync();
+            }
+            return responseMsg;
+        }
+
         [CustomAction]
         public static ActionResult FillList(Session xiSession)
         {
-
-            FillComboBox(xiSession, "US", "United States");
-            FillComboBox(xiSession, "CA", "Canada");
-            FillComboBox(xiSession, "MX", "Mexico");
-
+            try
+            {
+                string countryJson = getResponseAsync().Result;
+                if (!countryJson.Equals("Error"))
+                {
+                    dynamic stuff = JsonConvert.DeserializeObject(countryJson);
+                    foreach (dynamic country in stuff.servers)
+                    {
+                        FillComboBox(xiSession, (string)country.name, (string)country.alias);
+                    }
+                }
+                else /* TODO: Get this error out to screen */
+                {
+                    FillComboBox(xiSession, "South Africa", "zaf-server");
+                }
+            } catch (Exception e)   /* TODO: Get this error out to screen: e.ToString() */
+            {
+                FillComboBox(xiSession, "South Africa", "zaf-server");
+            }
             return ActionResult.Success;
         }
     }
